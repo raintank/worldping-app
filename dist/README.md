3104317ff7b57fb8100895e32c19340e1a435c6b
woodsaj <anthony@monkey.id.au>
2016-04-05 15:25:53 +0800

    add missing dist folder

---
+++ dist/README.md
@@ -0,0 +1,10 @@
+[worldPing](http://worldping.raintank.io/) is a plug-in for Grafana that continually tests, stores and alerts on the global performance and availability of your Internet applications so you can pinpoint issues, fix them immediately, and improve your user’s experience.
+
+**worldPing requires a [Grafana.net](http://grafana.net) account and [Grafana 3.x](http://grafana.org/download) to install.**
+
+## Documentation
+- [worldPing Usecases](http://worldping.raintank.io/worldping/use-cases)
+
+- [worldPing FAQ](http://worldping.raintank.io/worldping/faq)
+
+- [worldPing documentation](http://worldping.raintank.io/docs/)
---
+++ dist/components/probe/probe_list.js
@@ -0,0 +1,115 @@
+"use strict";
+
+System.register(["lodash"], function (_export, _context) {
+  var _, _createClass, ProbeListCtrl;
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  return {
+    setters: [function (_lodash) {
+      _ = _lodash.default;
+    }],
+    execute: function () {
+      _createClass = function () {
+        function defineProperties(target, props) {
+          for (var i = 0; i < props.length; i++) {
+            var descriptor = props[i];
+            descriptor.enumerable = descriptor.enumerable || false;
+            descriptor.configurable = true;
+            if ("value" in descriptor) descriptor.writable = true;
+            Object.defineProperty(target, descriptor.key, descriptor);
+          }
+        }
+
+        return function (Constructor, protoProps, staticProps) {
+          if (protoProps) defineProperties(Constructor.prototype, protoProps);
+          if (staticProps) defineProperties(Constructor, staticProps);
+          return Constructor;
+        };
+      }();
+
+      _export("ProbeListCtrl", ProbeListCtrl = function () {
+        /** @ngInject */
+
+        function ProbeListCtrl($scope, $injector, $location, $filter, backendSrv) {
+          _classCallCheck(this, ProbeListCtrl);
+
+          this.backendSrv = backendSrv;
+          this.$filter = $filter;
+          this.$location = $location;
+          this.pageReady = false;
+          this.statuses = [{ label: "Online", value: { online: true, enabled: true }, id: 2 }, { label: "Offline", value: { online: false, enabled: true }, id: 3 }, { label: "Disabled", value: { enabled: false }, id: 4 }];
+
+          this.filter = { tag: "", status: "" };
+          this.sort_field = "name";
+          this.collectors = [];
+          this.getCollectors();
+        }
+
+        _createClass(ProbeListCtrl, [{
+          key: "collectorTags",
+          value: function collectorTags() {
+            var self = this;
+            var map = {};
+            _.forEach(this.collectors, function (collector) {
+              _.forEach(collector.tags, function (tag) {
+                map[tag] = true;
+              });
+            });
+            return Object.keys(map);
+          }
+        }, {
+          key: "setCollectorFilter",
+          value: function setCollectorFilter(tag) {
+            this.filter.tag = tag;
+          }
+        }, {
+          key: "statusFilter",
+          value: function statusFilter() {
+            var self = this;
+            return function (actual) {
+              if (!self.filter.status) {
+                return true;
+              }
+              var res = self.$filter('filter')([actual], self.filter.status);
+              return res.length > 0;
+            };
+          }
+        }, {
+          key: "getCollectors",
+          value: function getCollectors() {
+            var self = this;
+            this.backendSrv.get('api/plugin-proxy/raintank-worldping-app/api/collectors').then(function (collectors) {
+              self.pageReady = true;
+              self.collectors = collectors;
+            });
+          }
+        }, {
+          key: "remove",
+          value: function remove(loc) {
+            var self = this;
+            this.backendSrv.delete('api/plugin-proxy/raintank-worldping-app/api/collectors/' + loc.id).then(function () {
+              self.getCollectors();
+            });
+          }
+        }, {
+          key: "gotoDashboard",
+          value: function gotoDashboard(collector) {
+            this.$location.path("/dashboard/db/worldping-collector-summary").search({ "var-collector": collector.slug, "var-endpoint": "All" });
+          }
+        }]);
+
+        return ProbeListCtrl;
+      }());
+
+      ProbeListCtrl.templateUrl = 'public/plugins/raintank-worldping-app/components/probe/partials/probe_list.html';
+
+      _export("ProbeListCtrl", ProbeListCtrl);
+    }
+  };
+});
+//# sourceMappingURL=probe_list.js.map
