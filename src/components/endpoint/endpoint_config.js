7e3534304b4d3e4fd19048177b38d6f310845d02
Matt Toback <matt@raintank.io>
2016-09-14 16:28:30 -0400

    Implemented the new login in the CTA panel, tested across multiple endpoint types

--- src/components/endpoint/endpoint_config.js
+++ src/components/endpoint/endpoint_config.js
@@ -257,7 +257,7 @@ class EndpointConfigCtrl {
       return 0;
     }
 
-    return (30.5 * 24 * (3600/check.frequency) * probeCount / 1000000) + 0.5;
+    return (30.5 * 24 * (3600/check.frequency) * probeCount / 1000000);
   }
 
   requiresUpgrade() {
