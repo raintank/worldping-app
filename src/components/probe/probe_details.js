2eb0b86c7c10b1b23aa8b883de726bab660caa0a
woodsaj <awoods@raintank.io>
2017-03-31 03:40:07 +0800

    correct worldping-events iframe url. fixes #188

--- src/components/probe/probe_details.js
+++ src/components/probe/probe_details.js
@@ -94,7 +94,7 @@ export class ProbeDetailsCtrl {
       return "";
     }
     var path = "/dashboard-solo/db/worldping-events";
-    var qstring = "?panelId=2&fullscreen&from=now-1d&to=now&var-probe="+this.probe.slug;
+    var qstring = "?panelId=2&from=now-1d&to=now&var-probe="+this.probe.slug;
     return path + qstring;
   }
 
